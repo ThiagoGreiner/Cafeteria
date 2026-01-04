60c424a6c9f66a2f83afbe60c452159d83294426
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-04 11:41:57 -0300

    Criando e reutilizando carousel

---
+++ backend-laravel/resources/views/components/carousel.blade.php
@@ -0,0 +1,38 @@
+<header id="coffee-shop" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#coffee-shop" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#coffee-shop" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#coffee-shop" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="https://images.unsplash.com/photo-1692299108318-5a6650eb6ae7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="Grãos de café">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Os melhores grãos</h5>
+            <p>Trabalhamos com igredientes selecionados, da melhor qualidade, do jeito que você merece</p>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1527596428171-7885b82c91c6?q=80&w=1937&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="Fachada da cafeteria">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Um ambiente pensado para você e sua família</h5>
+            <p>Nossa cafeteria foi pensada para te fornecer um ambiente confortável, tranquilo e leve</p>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1446321423766-c339f030bd0a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="Xicara de café">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>A melhor experiência gastronômica de café</h5>
+            <p>Você vai experimentar os melhores cafés do Brasil e do mundo</p>
+        </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#coffee-shop" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#coffee-shop" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</header>
