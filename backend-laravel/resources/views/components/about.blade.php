2b07bb367d77d971f57347943f809d1147226b46
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-04 11:51:04 -0300

    Criando e adicionando o sobre

---
+++ backend-laravel/resources/views/components/about.blade.php
@@ -0,0 +1,36 @@
+<section id="about" class="container">
+    <div class="row">
+        <div class="col-lg-6 col-md-12">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+            <div class="col-md-4">
+                <img src="https://images.unsplash.com/photo-1485808191679-5f86510681a2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid rounded-start" alt="Xicara de café">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">Somos apaixonados por café</h5>
+                <h6 class="card-subtitle text-muted mb-3">Acreditamos que o café pode mudar o (seu) mundo</h6>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nulla optio corrupti ipsum iure nostrum, nobis ut voluptatem odio ex labore ipsam, error atque sint tenetur, blanditiis omnis soluta fugit?</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="col-lg-6 col-md-12">
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+            <div class="col-md-4">
+                <img src="https://images.unsplash.com/photo-1620360289812-0abdae69d6d2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid rounded-start" alt="Barista colocando leite em café gelado">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">Equipe premium de baristas</h5>
+                <h6 class="card-subtitle text-muted mb-3">Profíssionais especializados para levar o melhor café até a sua mesa</h6>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nulla optio corrupti ipsum iure nostrum, nobis ut voluptatem odio ex labore ipsam, error atque sint tenetur, blanditiis omnis soluta fugit?</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
