dfe513e46c60022ce913ee453e8f42d4463f3a31
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-04 17:41:47 -0300

    Alterando menu para dinâmico

--- backend-laravel/resources/views/components/menu.blade.php
+++ backend-laravel/resources/views/components/menu.blade.php
@@ -1,156 +1,61 @@
 <section id="menu" class="container mt-5 mb-5">
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
-        <button class="nav-link active" id="espresso-tab" data-bs-toggle="tab" data-bs-target="#espresso-tab-pane" type="button" role="tab" aria-controls="espresso-tab-pane" aria-selected="true">Expressos</button>
+            <button class="nav-link active" id="espresso-tab" data-bs-toggle="tab" data-bs-target="#espresso-tab-pane" type="button" role="tab" aria-controls="espresso-tab-pane" aria-selected="true">Expressos</button>
         </li>
+
         <li class="nav-item" role="presentation">
-        <button class="nav-link" id="cappuccino-tab" data-bs-toggle="tab" data-bs-target="#cappuccino-tab-pane" type="button" role="tab" aria-controls="cappuccino-tab-pane" aria-selected="false">Capuccinos</button>
+            <button class="nav-link" id="cappuccino-tab" data-bs-toggle="tab" data-bs-target="#cappuccino-tab-pane" type="button" role="tab" aria-controls="cappuccino-tab-pane" aria-selected="false">Capuccinos</button>
         </li>
+        
         <li class="nav-item" role="presentation">
-        <button class="nav-link" id="iced-coffee-tab" data-bs-toggle="tab" data-bs-target="#iced-coffee-tab-pane" type="button" role="tab" aria-controls="iced-coffee-tab-pane" aria-selected="false">Gelados</button>
+            <button class="nav-link" id="iced-coffee-tab" data-bs-toggle="tab" data-bs-target="#iced-coffee-tab-pane" type="button" role="tab" aria-controls="iced-coffee-tab-pane" aria-selected="false">Gelados</button>
         </li>
+
         <li class="nav-item" role="presentation">
-        <button class="nav-link" id="specials-coffee-tab" data-bs-toggle="tab" data-bs-target="#specials-coffee-tab-pane" type="button" role="tab" aria-controls="specials-coffee-tab-pane" aria-selected="false">Especiais</button>
+            <button class="nav-link" id="specials-coffee-tab" data-bs-toggle="tab" data-bs-target="#specials-coffee-tab-pane" type="button" role="tab" aria-controls="specials-coffee-tab-pane" aria-selected="false">Especiais</button>
         </li>
     </ul>
     
     <div class="tab-content" id="coffeesTabContent">
         <div class="tab-pane fade show active" id="espresso-tab-pane" role="tabpanel" aria-labelledby="espresso-tab" tabindex="0">
-        <!-- Coffe card -->
-        <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://plus.unsplash.com/premium_photo-1675435644687-562e8042b9db?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FmJUMzJUE5JTIwZXhwcmVzc298ZW58MHwxfDB8fHww" class="card-img-top" alt="Café Simples">
-                <div class="card-body">
-                <h5 class="card-title">Café Expresso Simples</h5>
-                <p class="card-text">R$ 5,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1596952954288-16862d37405b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FmJUMzJUE5JTIwZXhwcmVzc298ZW58MHwxfDB8fHww" class="card-img-top" alt="Café Duplo">
-                <div class="card-body">
-                <h5 class="card-title">Café Expresso Duplo</h5>
-                <p class="card-text">R$ 7,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://plus.unsplash.com/premium_photo-1693266634755-c7cc7aa8de58?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y2FmJUMzJUE5JTIwZXhwcmVzc298ZW58MHwxfDB8fHww" class="card-img-top" alt="Café Forte">
-                <div class="card-body">
-                <h5 class="card-title">Café Expresso Forte</h5>
-                <p class="card-text">R$ 6,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://plus.unsplash.com/premium_photo-1669687924558-386bff1a0469?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FmJUMzJUE5JTIwZXhwcmVzc298ZW58MHwxfDB8fHww" class="card-img-top" alt="Café Fraco">
-                <div class="card-body">
-                <h5 class="card-title">Café Expresso Fraco</h5>
-                <p class="card-text">R$ 3,00</p>
-                </div>
-            </div>
+            <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+
+                @forelse ($coffees['espresso'] ?? [] as $coffee)
+                    @include('components.coffee-card', compact('coffee'))
+                @endforeach
+
             </div>
         </div>
-        </div>
+
         <div class="tab-pane fade" id="cappuccino-tab-pane" role="tabpanel" aria-labelledby="cappuccino-tab" tabindex="0">
-        <!-- Coffe card -->
-        <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1730411317769-b991ec3951d5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2FmJUMzJUE5JTIwY2FwdWNjaW5vfGVufDB8MXwwfHx8MA%3D%3D" class="card-img-top" alt="Capuccino Simples">
-                <div class="card-body">
-                <h5 class="card-title">Capuccino Simples</h5>
-                <p class="card-text">R$ 12,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1708430651927-20e2e1f1e8f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmJUMzJUE5JTIwY2FwdWNjaW5vfGVufDB8MXwwfHx8MA%3D%3D" class="card-img-top" alt="Capuccino Especial">
-                <div class="card-body">
-                <h5 class="card-title">Capuccino Especial</h5>
-                <p class="card-text">R$ 15,00</p>
-                </div>
-            </div>
+            <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+
+                @forelse ($coffees['cappuccino'] ?? [] as $coffee)
+                    @include('components.coffee-card', compact('coffee'))
+                @endforeach
+
             </div>
         </div>
-        </div>
+
         <div class="tab-pane fade" id="iced-coffee-tab-pane" role="tabpanel" aria-labelledby="iced-coffee-tab" tabindex="0">
-        <!-- Coffe card -->
-        <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1531835207745-506a1bc035d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FmJUMzJUE5JTIwZ2VsYWRvc3xlbnwwfDF8MHx8fDA%3D" class="card-img-top" alt="Café Gelado Simples">
-                <div class="card-body">
-                <h5 class="card-title">Café Gelado Simples</h5>
-                <p class="card-text">R$ 18,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1514189347135-3e90a3b561f7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmJUMzJUE5JTIwZ2VsYWRvc3xlbnwwfDF8MHx8fDA%3D" class="card-img-top" alt="Café Gelado com Chocolate">
-                <div class="card-body">
-                <h5 class="card-title">Café Gelado com Chocolate</h5>
-                <p class="card-text">R$ 19,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1515316416554-1af1fbca57a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FmJUMzJUE5JTIwZ2VsYWRvc3xlbnwwfDF8MHx8fDA%3D" class="card-img-top" alt="Café Gelado Especial">
-                <div class="card-body">
-                <h5 class="card-title">Café Gelado Especial</h5>
-                <p class="card-text">R$ 22,00</p>
-                </div>
-            </div>
+            <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+
+                @forelse ($coffees['iced'] ?? [] as $coffee)
+                    @include('components.coffee-card', compact('coffee'))
+                @endforeach
+
             </div>
         </div>
-        </div>
+
         <div class="tab-pane fade" id="specials-coffee-tab-pane" role="tabpanel" aria-labelledby="specials-coffee-tab" tabindex="0">
-        <!-- Coffe card -->
-        <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1586792106950-8f02ebb373c1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FmJUMzJUE5JTIwZXNwZWNpYWlzfGVufDB8MXwwfHx8MA%3D%3D" class="card-img-top" alt="Café da Casa">
-                <div class="card-body">
-                <h5 class="card-title">Café da Casa</h5>
-                <p class="card-text">R$ 22,00</p>
-                </div>
+            <div class="row row-cols-1 row-cols-md-3 g-4 mt-3">
+
+                @forelse ($coffees['special'] ?? [] as $coffee)
+                    @include('components.coffee-card', compact('coffee'))
+                @endforeach
+
             </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://plus.unsplash.com/premium_photo-1669905375164-388815c9dcf6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FmJUMzJUE5JTIwZXNwZWNpYWlzJTIwYmViaWRhfGVufDB8MXwwfHx8MA%3D%3D" class="card-img-top" alt="Café de Torra Áfricana">
-                <div class="card-body">
-                <h5 class="card-title">Torra Áfricana</h5>
-                <p class="card-text">R$ 25,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://plus.unsplash.com/premium_photo-1723559972702-2659e41dbb5b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNhZiVDMyVBOSUyMGVzcGVjaWFpcyUyMGJlYmlkYXxlbnwwfDF8MHx8fDA%3D" class="card-img-top" alt="Café Alemão">
-                <div class="card-body">
-                <h5 class="card-title">Torra Alemã</h5>
-                <p class="card-text">R$ 25,00</p>
-                </div>
-            </div>
-            </div>
-            <div class="col">
-            <div class="card card-menu">
-                <img src="https://images.unsplash.com/photo-1500217052183-bc01eee1a74e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fGNhZiVDMyVBOSUyMGVzcGVjaWFpcyUyMGJlYmlkYXxlbnwwfDF8MHx8fDA%3D" class="card-img-top" alt="Café Irlandes">
-                <div class="card-body">
-                <h5 class="card-title">Torra Irlandesa</h5>
-                <p class="card-text">R$ 28,00</p>
-                </div>
-            </div>
-            </div>
-        </div>
         </div>
     </div>
 </section>
