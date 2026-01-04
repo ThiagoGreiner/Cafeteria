122b76136ca05d7779bb0ba5aa1c7a54541cb442
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-04 11:13:03 -0300

    Criando pagina inicial

---
+++ backend-laravel/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+
+<section class="container mt-5 pt-5">
+    <h1>Bem-vindo à Best Coffee</h1>
+    <p>
+        Transformando sua experiência com café.
+    </p>
+</section>
+
+@endsection
