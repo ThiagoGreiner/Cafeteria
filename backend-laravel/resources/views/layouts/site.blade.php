954f2d468f16e00735458780276cd34227cab8d4
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-06 22:35:28 -0300

    Recriandro estrutura da pagina publica home

---
+++ backend-laravel/resources/views/layouts/site.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Best Coffee')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+<body>
+
+    @include('components.navbar')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('components.footer')
+
+    <script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+    </script>
+</body>
+</html>
