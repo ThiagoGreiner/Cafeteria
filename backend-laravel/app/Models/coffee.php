60d379987eba97ddfa79ae24ca64cea971687545
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-09 18:41:49 -0300

    Ajuste de ordem category e image

--- backend-laravel/app/Models/coffee.php
+++ backend-laravel/app/Models/coffee.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class coffee extends Model
+class Coffee extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'name',
         'price',
+        'category',
         'image',
-        'category'
     ];
 }
