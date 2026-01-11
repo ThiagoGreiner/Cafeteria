5d74f9d35c9b2cfdef0fc4f580dfb5ac05c60b7c
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-11 09:07:25 -0300

    Adiciona logo e altera descrições menu

--- backend-laravel/resources/views/components/admin-navbar.blade.php
+++ backend-laravel/resources/views/components/admin-navbar.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Offcanvas dark navbar</a>
+
+    <a class="navbar-brand" href="#"><img src="{{ asset('assets/img/logo.png') }}" alt="Logotipo Cafeteria"></a>
     
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -8,7 +9,7 @@
     
     <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
 
@@ -23,7 +24,7 @@
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <button class="nav-link btn btn-link" type="submit">
-                    Logout
+                    Sair
                 </button>
             </form>
           </li>
