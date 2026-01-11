58556693bfc2cc3511dbc3ead3b2468076dfe145
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-11 09:08:09 -0300

    Adiciona submenu

--- backend-laravel/resources/views/components/admin-sidebar.php
+++ backend-laravel/resources/views/components/admin-sidebar.php
@@ -1,13 +1,31 @@
 <ul class="nav flex-column">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
+    <li class="nav-item">
+        <a class="nav-link text-white d-flex justify-content-between align-items-center" data-bs-toggle="collapse" href="#usuariosMenu" role="button" aria-expanded="false" aria-controls="usuariosMenu">
+            Cafés
+            <span class="ms-2">▾</span>
+        </a>
 
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
+        <!-- SubMenu -->
+        <div class="collapse" id="usuariosMenu">
+            <ul class="nav flex-column ms-3">
+                <li class="nav-item">
+                    <a class="nav-link text-white-50" href="#">Listar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white-50" href="#">Criar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white-50" href="#">Editar</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-white" href="#">Pedidos</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link text-white" href="#">Caixa</a>
+    </li>
 </ul>
