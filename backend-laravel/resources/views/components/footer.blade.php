00ab9be8f2fa13bb14d7d3f99a0f980de3887fee
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-11 09:08:35 -0300

    Retira margin do footer

--- backend-laravel/resources/views/components/footer.blade.php
+++ backend-laravel/resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
- <footer class="text-center bg-dark p-5 mt-5 text-light">
+ <footer class="text-center bg-dark p-5 text-light">
       <img src="{{ asset('assets/img/logo.png') }}" alt="Logotipo Best Coffee">
       
       <p class="mt-2 mb-0 fw-bold">
