f7f26b22e6c8b9b285e0580b50a07bb1af84296f
Thiago Greiner Rodrigues <thiagogreiner0@gmail.com>
2026-01-11 21:40:01 -0300

    Adiciona ações de admin

--- backend-laravel/resources/views/components/coffee-card.blade.php
+++ backend-laravel/resources/views/components/coffee-card.blade.php
@@ -1,11 +1,29 @@
 <div class="col">
     <div class="card card-menu">
         <img src="{{ asset('storage/' . $coffee->image) }}" class="card-img-top">
+        
         <div class="card-body">
             <h5 class="card-title">{{ $coffee->name }}</h5>
             <p class="card-text">
                 R$ {{ number_format($coffee->price, 2, ',', '.') }}
             </p>
+
+            <!-- Ações admin -->
+            @isset($admin)
+                <div class="mt-auto d-flex gap-2">
+                    <a href="{{ route('admin.coffees.edit', $coffee) }}" class="btn btn-sm btn-warning w-100">
+                        Editar
+                    </a>
+
+                    <form action="{{ route('admin.coffees.destroy', $coffee) }}" method="POST" class="w-100" onsubmit="return confirm('Deseja excluir este café?')">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-sm btn-danger w-100">
+                            Excluir
+                        </button>
+                    </form>
+                </div>
+            @endisset
         </div>
     </div>
 </div>
